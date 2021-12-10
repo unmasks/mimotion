effcf2610e704a715443300ff9ab3db36c667f0d
unmask <yuygfly@126.com>
2021-12-10 23:14:05 +0800

    [feat]vqq

---
+++ vqq.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+"""
+@author: sy-records
+@license: https://github.com/sy-records/v-checkin/blob/master/LICENSE
+@contact: 52o@qq52o.cn
+@desc: 腾讯视频好莱坞会员V力值签到，支持两次签到：一次正常签到，一次手机签到。
+@blog: https://qq52o.me
+"""
+
+# python
+import requests
+# req = requests.session()
+# req.keep_alive = False
+import urllib3
+
+urllib3.disable_warnings()
+
+auth_refresh_url = "https://access.video.qq.com/user/auth_refresh?vappid=11059694&vsecret=fdf61a6be0aad57132bc5cdf78ac30145b6cd2c1470b0cfe&type=qq&g_tk=&g_vstk=973910225&g_actk=968279508&callback=jQuery19109822469403028486_1639144467219&_=1639144467220"
+sckey = "SCT102724TTVMjMKTYZP7qeiFpH7XTaIPL"
+
+ftqq_url = "https://sctapi.ftqq.com/%s.send" % (sckey)
+url1 = "https://vip.video.qq.com/fcgi-bin/comm_cgi?name=hierarchical_task_system&cmd=2"
+url2 = "https://v.qq.com/x/bu/mobile_checkin"
+
+login_headers = {
+    "Referer": "https://v.qq.com",
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+    "Cookie": "tvfe_boss_uuid=9e6fda1f6e0aef6c; video_guid=8da97668983f2553; video_platform=2; pgv_pvid=7083940873; pgv_info=ssid=s7584507632; _qpsvr_localtk=0.7789290835782194; RK=uUYwDdOaHO; ptcz=32a3e4fec1adb0ad36649af91a142d1b4795fd412017c4617257a865d1fd3bf3; main_login=qq; vqq_access_token=B2145F56D3B5F349360789F2FC815A8C; vqq_appid=101483052; vqq_openid=C451041BD2DBF20318039E0BAA75C1E1; vqq_vuserid=237354545; vqq_vusession=-FJtpRMOnoc0Frzk4KLagA..; vqq_refresh_token=5B46657C33D4D5E753CCCF037F50D63D; vqq_next_refresh_time=6600; vqq_login_time_init=1639144464; login_time_init=2021-12-10 21:54:25; uid=554832033;",
+}
+
+login = requests.get(url=auth_refresh_url, headers=login_headers)
+cookie = requests.utils.dict_from_cookiejar(login.cookies)
+
+if not cookie:
+    print("auth_refresh error")
+    payload = {"title": "腾讯视频V力值签到通知", "desp": "获取Cookie失败，Cookie失效"}
+    requests.post(ftqq_url, params=payload)
+    exit
+
+sign_headers = {
+    "Cookie": "tvfe_boss_uuid=9e6fda1f6e0aef6c; video_guid=8da97668983f2553; video_platform=2; pgv_pvid=7083940873; pgv_info=ssid=s7584507632; _qpsvr_localtk=0.7789290835782194; RK=uUYwDdOaHO; ptcz=32a3e4fec1adb0ad36649af91a142d1b4795fd412017c4617257a865d1fd3bf3; main_login=qq; vqq_access_token=B2145F56D3B5F349360789F2FC815A8C; vqq_appid=101483052; vqq_openid=C451041BD2DBF20318039E0BAA75C1E1; vqq_vuserid=237354545; vqq_refresh_token=5B46657C33D4D5E753CCCF037F50D63D; vqq_next_refresh_time=6600; vqq_login_time_init=1639144464; login_time_init=2021-12-10 21:54:25; uid=554832033; vqq_vusession="
+    + cookie["vqq_vusession"]
+    + ";",
+    "Referer": "https://m.v.qq.com",
+}
+
+
+def start():
+    sign1 = requests.get(url1, headers=sign_headers).text
+    if "Account Verify Error" in sign1:
+        print("Sign1 error,Cookie Invalid")
+        status = "链接1 失败，Cookie失效"
+    else:
+        print("Sign1 Success")
+        status = "链接1 成功，获得V力值：" + sign1[42:-14]
+
+    sign2 = requests.get(url2, headers=sign_headers).text
+    if "Unauthorized" in sign2:
+        print("Sign2 error,Cookie Invalid")
+        status = status + "\n\n 链接2 失败，Cookie失效"
+    else:
+        print("Sign2 Success")
+        status = status + "\n\n 链接2 成功"
+
+    payload = {"title": "腾讯视频V力值签到通知", "desp": status}
+    requests.post(ftqq_url, params=payload)
+
+
+def main_handler(event, context):
+    return start()
+
+
+if __name__ == "__main__":
+    start()
